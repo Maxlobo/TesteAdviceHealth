5e055d3194ebf0f7e00f4d5c9e8df3ed6e1b6393
Max <maxhb.df@gmail.com>
2025-07-20 13:08:02 -0300

    Add login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,58 @@
-import api from '../services/api';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import "../styles/login.css";
 
-const handleLogin = async (username, password) => {
-    try {
-        const response = await api.post('login/', { username, password });
-    } catch (error) {
-        console.error('Login failed:', error.response.data);
-    }
-};
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+        try {
+            await login(username, password);
+            navigate("/");
+        } catch (error) {
+            setError(error.response || 'Login failed. Please verify your credentials.');
+        }
+    };
+
+    return (
+        <div className="login_container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div style={{ marginBottom: '15px' }}>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        className="input_username"
+                    />
+                </div>
+                <div style={{ marginBottom: '15px' }}>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        className="input_password"
+                    />
+                </div>
+                {error && <p style={{ color: 'red', marginBottom: '15px' }}>{error}</p>}
+                <button type="submit" className="login_button">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
